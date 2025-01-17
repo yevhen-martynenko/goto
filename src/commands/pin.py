1c73a60a0bbe32a00a059129dbb26f9f1c953852
Yevhen Martynenko <yevhen.martynenko.v@gmail.com>
2025-01-17 15:54:37 +0200

    feat(commands): implement pin functionality in pin.py

--- src/commands/pin.py
+++ src/commands/pin.py
@@ -1,2 +1,23 @@
-def add_pin(directory):
-    print(f'"{directory}" directory pinned')
+import os
+import sqlite3
+
+
+def add_pin(shortcut):
+    project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+    db_path = os.path.join(project_root, "data", "database.db")
+    directory_path = os.getcwd()
+
+    try:
+        with sqlite3.connect(db_path) as con:
+            cur = con.cursor()
+
+            cur.execute(
+                "INSERT INTO pins (directory_path, shortcut) VALUES (?, ?)",
+                (directory_path, shortcut),
+            )
+
+            con.commit()
+            print(f'"{shortcut}" shortcut added')
+
+    except sqlite3.Error as e:
+        print(f"Error while adding pin: {e}")
